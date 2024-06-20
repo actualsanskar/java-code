fda9375d2458d4697ac6310a439e1709f81c7103
Sanskar Agarwal <ytmarketor+github@gmail.com>
2024-06-20 16:58:03 +0530

    Added multidimentionalarray file

---
+++ Concept/multidimensionalArrays.java
@@ -0,0 +1,20 @@
+public class multidimensionalArrays {
+    public static void main(String[] args) {
+
+        int a[][]= new int[4][3];
+
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j<3; j++){
+                a[i][j] = (int)(Math.random() * 10);
+            }
+            
+        }
+
+        for(int i = 0; i<4; i++){
+            for(int j = 0; j<3; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
